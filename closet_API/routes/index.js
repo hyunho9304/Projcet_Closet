6919b237e00056923cda89c793d56d050ee01abb
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-07-03 23:32:22 +0900

    completion Project_Closet_Server

--- closet_API/routes/index.js
+++ closet_API/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+//	member 관련
 const member = require( './member/index' ) ;
 router.use( '/member' , member ) ;
 
+//	옷장 관련
 const closet = require( './closet/index') ;
 router.use( '/closet' , closet ) ;
 
